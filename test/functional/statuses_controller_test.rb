7e8d17ef0fe0c84969cb84eecb40350f83b9ec88
jason sigmon <jaytomsig@gmail.com>
2013-07-23 10:21:44 -0400

    Change Statuses and Scope

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -31,6 +31,7 @@ class StatusesControllerTest < ActionController::TestCase
    
   test "should create status when logged in" do
     sign_in users(:jason)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content}
     end
@@ -38,9 +39,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
+   test "should create status for the current user when logged in" do
+    sign_in users(:jason)
+
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:Jim).id}
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
+
   test "should show status" do
     get :show, id: @status
     assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
   end
 
   test "should get edit when logged in" do
@@ -59,6 +77,20 @@ class StatusesControllerTest < ActionController::TestCase
     sign_in users(:jason)
     put :update, id: @status, status: { content: @status.content}
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should update status for the current user when logged in" do
+    sign_in users(:jason)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:Jim).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
+
+  test "should not update status if nothing has changed" do
+    sign_in users(:jason)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
   end
 
   test "should destroy status" do
